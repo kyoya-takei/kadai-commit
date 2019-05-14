332615e3bec06df5be2f69cf543e54c2cf19999d
kyoya.takei <kyoya-takei@dac.co.jp>
2019-05-14 01:13:23 +0000

    add sample.php

---
+++ sample.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <haed>
+        <meta charset="utf-8">
+        <title>おみくじ</title>
+    </haed>
+    
+    <body>
+        <h1>おみくじ</h1>
+        <p>おみくじの結果：なし</p>
+        
+        <form action="sample.php" method="POST">
+            <button type="submit" name="act" value="draw">おみくじを引く！</button>
+        </form>
+    </body>
+    
+</html>
