386f4b09d7522ce591ea38bae253b68ac69674b9
kyoya.takei <kyoya-takei@dac.co.jp>
2019-05-14 06:51:52 +0000

    add sample.php

--- sample.php
+++ sample.php
@@ -1,34 +1,14 @@
-<?php
-require_once "functions.php";
-
-$result = "無し";
-
-if (array_key_exists("act", $_POST)) {
-    $result = omikuji();
-}
-
-function omikuiji() {
-    $fortune = ["大吉", "中吉", "小吉", "吉", "末吉", "凶"];
-    $rand_index = random_int(0, count($fortune)-1);
-    return $fortune[$rand_index];
-}
- 
-?>
-
 <!DOCTYPE html>
 <html lang="ja">
-    <haed>
-        <meta charset="utf-8">
+    <head>
+        <meta charset="UTF-8">
         <title>おみくじ</title>
-    </haed>
-    
+    </head>
     <body>
         <h1>おみくじ</h1>
-        <p>おみくじの結果：<?php print h($result); ?></p>
-        
+        <p>おみくじの結果：無し</p>
         <form action="sample.php" method="POST">
-            <button type="submit" name="act" value="draw">おみくじを引く！</button>
+            <button type="submit" name="act" value="draw">おみくじをひく！</button>
         </form>
     </body>
-    
 </html>
