1906fddc738dd8d7689eb59adfb804ed8777f83b
kyoya.takei <kyoya-takei@dac.co.jp>
2019-05-14 01:16:40 +0000

    update sample.php

--- sample.php
+++ sample.php
@@ -1,3 +1,10 @@
+<?php
+require_once "functions.php";
+
+$result = "無し";
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
     <haed>
@@ -7,7 +14,7 @@
     
     <body>
         <h1>おみくじ</h1>
-        <p>おみくじの結果：なし</p>
+        <p>おみくじの結果：<?php print h($result); ?></p>
         
         <form action="sample.php" method="POST">
             <button type="submit" name="act" value="draw">おみくじを引く！</button>
